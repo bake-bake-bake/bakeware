72f9d487650f14372db0e05eceef0b3585e3aa74
Connor Rigby <connorr@hey.com>
2020-09-13 10:47:31 -0700

    Add scenic app

---
+++ examples/scenic_app/lib/scenic_app.ex
@@ -0,0 +1,17 @@
+defmodule ScenicApp do
+  @moduledoc """
+  Starter application using the Scenic framework.
+  """
+
+  def start(_type, _args) do
+    # load the viewport configuration from config
+    main_viewport_config = Application.get_env(:scenic_app, :viewport)
+
+    # start the application with the viewport
+    children = [
+      {Scenic, viewports: [main_viewport_config]}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
