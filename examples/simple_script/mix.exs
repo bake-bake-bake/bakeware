f846cd1fcb14cdb544679fc88e038ec12279ee04
Connor Rigby <connorr@hey.com>
2020-09-13 11:41:50 -0700

    Add Script behaviour

--- examples/simple_script/mix.exs
+++ examples/simple_script/mix.exs
@@ -18,14 +18,15 @@ defmodule SimpleScript.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {SimpleScript, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:bakeware, path: "../../bakeware"}
+      {:bakeware, path: "../../bakeware", runtime: false}
     ]
   end
 
