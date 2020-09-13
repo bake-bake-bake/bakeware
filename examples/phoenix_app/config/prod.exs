f2d131a7560b13f99af2a4b976fde8db84ebeee9
Jon Carstens <jjcarstens@me.com>
2020-09-13 15:00:35 -0600

    dont check origin in prod

--- examples/phoenix_app/config/prod.exs
+++ examples/phoenix_app/config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :phoenix_app, PhoenixAppWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info
