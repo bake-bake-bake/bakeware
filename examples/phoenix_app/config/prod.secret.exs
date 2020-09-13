b8fc1ca9e56fe5199e5088a5ca5f8d20ed800b65
Connor Rigby <connorr@hey.com>
2020-09-13 10:56:04 -0700

    Fix Phoenix Example start

--- examples/phoenix_app/config/prod.secret.exs
+++ examples/phoenix_app/config/prod.secret.exs
@@ -4,19 +4,15 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = "heCgjeUnxh8VPFkS2j1ruY7K/dvl5GQ/uCum72/Uohpwe/fyW23fQs2AS7zHuw6z"
 
 config :phoenix_app, PhoenixAppWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  server: true
 
 # ## Using releases (Elixir v1.9+)
 #
