6f214f7a2a797bf84d13aa8758773980fd49c799
Frank Hunleth <fhunleth@troodon-software.com>
2020-09-13 19:08:47 -0400

    Add examples to create-bin

--- examples/create_bins.sh
+++ examples/create_bins.sh
@@ -8,7 +8,7 @@
 EXAMPLES_DIR=$PWD
 OS=$(uname -s)
 
-if [ "$OS" = "Darwin" ]; then 
+if [ "$OS" = "Darwin" ]; then
   OS="MacOS"
 fi;
 
@@ -16,13 +16,15 @@ BIN_DIR=$EXAMPLES_DIR/bin/$OS
 
 mkdir -p $BIN_DIR
 
-for app in phoenix_app scenic_app simple_app simple_script
+for app in phoenix_app scenic_app simple_app simple_script iex_prompt nif_script
 do
 echo "*** Building $app bin ***"
 
 cd $EXAMPLES_DIR/$app
 rm -rf _build
-MIX_ENV=prod mix do deps.get, release
+MIX_ENV=prod mix deps.get
+MIX_ENV=prod mix setup
+MIX_ENV=prod mix release
 
 echo "*** copying $app bin to examples/bin/$OS ***\n"
 cp _build/prod/rel/bakeware/* $BIN_DIR
