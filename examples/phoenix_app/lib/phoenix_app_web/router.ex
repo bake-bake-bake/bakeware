5b66c9485a54a1583ca252c310d23b329f61f54e
Connor Rigby <connorr@hey.com>
2020-09-13 10:56:04 -0700

    Fix Phoenix Example start

--- examples/phoenix_app/lib/phoenix_app_web/router.ex
+++ examples/phoenix_app/lib/phoenix_app_web/router.ex
@@ -26,17 +26,10 @@ defmodule PhoenixAppWeb.Router do
 
   # Enables LiveDashboard only for development
   #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: PhoenixAppWeb.Telemetry
-    end
+  scope "/" do
+    pipe_through :browser
+    live_dashboard "/dashboard", metrics: PhoenixAppWeb.Telemetry
   end
 end
