e93a270f567d4ebde0d0683e38fce7cec00627e6
Jon Carstens <jjcarstens@me.com>
2020-09-13 13:36:06 -0600

    move binaries to system dir in bin

--- examples/create_bins.sh
+++ examples/create_bins.sh
@@ -5,11 +5,14 @@
 #
 # Then these executables can be called directly without needing compilation in an Elixir/OTP env
 
-APPS=(phoenix_app scenic_app simple_app simple_script)
-
 EXAMPLES_DIR=$PWD
+OS=$(uname -s)
 
-for app in "${APPS[@]}"
+if [ "$OS" = "Darwin" ]; then 
+  OS="MacOS"
+fi;
+
+for app in phoenix_app scenic_app simple_app simple_script
 do
 echo -e "*** Building $app bin ***"
 
@@ -17,8 +20,8 @@ cd $EXAMPLES_DIR/$app
 rm -rf _build
 mix release
 
-echo -e "*** copying $app bin to examples/bin ***"
-cp _build/prod/rel/bakeware/* $EXAMPLES_DIR/bin/
+echo -e "*** copying $app bin to examples/bin/$OS ***"
+cp _build/prod/rel/bakeware/* $EXAMPLES_DIR/bin/$OS
 
 echo -e "*** Finished $app ****\n"
 done
