f6f26580fbb6b0cd584e507255e0b11b31b60612
Connor Rigby <connorr@hey.com>
2020-09-13 10:47:31 -0700

    Add scenic app

---
+++ examples/scenic_app/mix.exs
@@ -0,0 +1,42 @@
+defmodule ScenicApp.MixProject do
+  use Mix.Project
+  @app :scenic_app
+
+  def project do
+    [
+      app: @app,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      build_embedded: true,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      releases: [{@app, release()}]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {ScenicApp, []},
+      extra_applications: []
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:scenic, "~> 0.10"},
+      {:scenic_driver_glfw, "~> 0.10", targets: :host},
+      {:bakeware, path: "../../bakeware", runtime: false}
+    ]
+  end
+
+  defp release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      steps: [:assemble, &Bakeware.assemble/1],
+      strip_beams: Mix.env() == :prod
+    ]
+  end
+end
