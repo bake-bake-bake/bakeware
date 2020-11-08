ca8b1da2668d0fc988c7977910f36d6670ea5a9f
Frank Hunleth <fhunleth@troodon-software.com>
2020-11-08 11:00:12 -0500

    Handle systems that don't define OPEN_MAX

--- src/rm_fr.c
+++ src/rm_fr.c
@@ -1,8 +1,18 @@
 #include "bakeware.h"
 
 #include <ftw.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
+
+// OPEN_MAX isn't defined on some systems. Note that many nftw man pages say
+// that the max open file handles parameter is ignored, but it's definitely not
+// ignored in the musl libc. On brief inspection, it looks like the number of
+// handles needed is related to the maximum directory depth, so this value
+// probably doesn't need to be that large in practice.
+#ifndef OPEN_MAX
+#define OPEN_MAX 255
+#endif
 
 static int rm(const char *path, const struct stat *s, int flag, struct FTW *ftw)
 {
